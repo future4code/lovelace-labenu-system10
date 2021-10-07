67bf4ac47d6ac944bfe73cab0119d1194b28f62b
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-10-07 20:15:39 -0300

    funcao de pegar estudante pelo id

--- src/repository/studants.ts
+++ src/repository/studants.ts
@@ -2,12 +2,18 @@ import { connection } from "../database/connection";
 import { Studant } from "../types";
 
 export const createStydant = async (studant: Studant) => {
-    await connection
-        .insert({
-            id: studant.id,
-            name: studant.name,
-            email: studant.email,
-            birth_date: studant.birthDate,
-            class_id: studant.class_id
-        })
+  await connection.insert({
+    id: studant.id,
+    name: studant.name,
+    email: studant.email,
+    birth_date: studant.birthDate,
+    class_id: studant.class_id,
+  });
+};
+
+export const getStudantAgeByStudantId = async (studandId: number) => {
+    const studant = await connection('studant')
+                        .where({id: studandId})
+
+    return studant
 }
