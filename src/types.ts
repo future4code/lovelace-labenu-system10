2bf59ca0fae7ef209146e861f6d0bcd779e5ec1c
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-10-06 22:26:52 -0300

    types studant, teacher e class

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+
+export type Studant = {
+    id: string,
+    name: string,
+    email: string,
+    birthDate: Date
+}
+
+export type Teacher = {
+    id: string,
+    name: string,
+    email: string,
+    birthDate: Date
+}
+
+export type Class = {
+    id: string,
+    name: string,
+    startDate: Date,
+    endDate: Date
+}
