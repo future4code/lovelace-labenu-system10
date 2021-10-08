579f94fc948f22d2017a68fdbf408fe7dd19013f
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-10-07 21:30:45 -0300

    seed

---
+++ src/database/data.ts
@@ -0,0 +1,52 @@
+export const data = {
+    "class": [
+        {
+            "id": 0,
+            "name": "Lovelace-na-night",
+            "module": 5,
+            "start_date": "2021-04-05",
+            "end_date": "2021-12-25"
+        },
+        {
+            "id": 1,
+            "name": "Dubalacubacu-na-night",
+            "module": 6,
+            "start_date": "2021-01-02",
+            "end_date": "2021-09-15"
+        }
+    ],
+
+    "studant": [
+        {
+            "id": 0,
+            "name": "Tiago",
+            "email": "tiago@email.com",
+            "birth_date": "1992-04-23",
+            "class_id": 0
+        },
+        {
+            "id": 1,
+            "name": "Gustavo",
+            "email": "gustavo@email.com",
+            "birth_date": "1992-04-30",
+            "class_id": 1
+        }
+    ],
+
+    "teacher": [
+        {
+            "id": 0,
+            "name": "Matheus",
+            "email": "matheus@email.com",
+            "birth_date": "1932-05-21",
+            "class_id": 0
+        },
+        {
+            "id": 1,
+            "name": "Amanda",
+            "email": "amanda@email.com",
+            "birth_date": "1995-02-12",
+            "class_id": 1
+        }
+    ]
+}
